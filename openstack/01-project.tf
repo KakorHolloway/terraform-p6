134ecfc60ac38dbd51837292063f9d05fa0920fb
Kakor <benjamin.belaud@hotmail.com>
2025-12-17 11:02:31 +0100

    set upper function

--- openstack/01-project.tf
+++ openstack/01-project.tf
@@ -1,4 +1,8 @@
 resource "openstack_identity_project_v3" "ipi-project" {
-  name        = "TERRAFORM${var.env}"
+  name        = upper("TERRAFORM${var.env}")
   description = "Projet Terraform pour infr050"
+}
+
+output "tenant_id" {
+  value = openstack_identity_project_v3.ipi-project.id
 }
